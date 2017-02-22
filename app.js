1ac2715c1d5b34a37ebe9fbb9a8f73708479eab6
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-02-22 10:50:01 -0800

    Add GroceryList component
    Add Pie and IceCream components

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+
+var Pie = () => (
+  <li>Pie</li>
+);
+
+var IceCream = () => (
+  <li>Ice Cream</li>
+);
 // TODO
+var GroceryList = () => (
+  <ul>
+	  <Pie />
+	  <IceCream />
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
