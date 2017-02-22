54839a66b7c62ce875823c4f048832e02dc98c3e
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-02-22 11:32:23 -0800

    Refactor GroceryList to dynamically render
    Create reusable GroceryListItem

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 
-var Pie = () => (
-  <li>Pie</li>
-);
-
-var IceCream = () => (
-  <li>Ice Cream</li>
-);
-// TODO
-var GroceryList = () => (
+var GroceryList = (props) => (
   <ul>
-	  <Pie />
-	  <IceCream />
+  {props.items.map( item => <GroceryListItem item={item} />)}
   </ul>
 );
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+ReactDOM.render(<GroceryList items = {['pie', 'tea', 'food']}/>, document.getElementById("app"));
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+// var Pie = () => (
+//   <li>Pie</li>
+// );
+
+// var IceCream = () => (
+//   <li>Ice Cream</li>
+// );
+// // TODO
+// var GroceryList = (props) => (
+//   <ul>
+// 	  <Pie />
+// 	  <IceCream />
+//   </ul>
+// );
